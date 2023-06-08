31dc5404fc9493439c58edfc466100849168e4c4
Max Lopez <familiacachorin@gmail.com>
2023-06-08 14:17:27 -0700

    Create playRound function

--- game.js
+++ game.js
@@ -11,3 +11,51 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            switch (computerSelection) {
+                case "Rock":
+                    return "tie you both chose rock"
+                    break;
+                case "Paper":
+                    return "You lose! paper beats rock"
+                    break;
+                default:
+                    return "You win! rock beats scissors"
+            }
+            break;
+        case "paper":
+            switch (computerSelection) {
+                case "Rock":
+                    return "You win! paper beats rock"
+                    break;
+                case "Paper":
+                    return "tie you both chose paper"
+                    break;
+                default:
+                    return "You lose, Scissors beats paper"
+            }
+            break;
+        case "scissors":
+            switch (computerSelection) {
+                case "Rock":
+                    return "You lose, rock beats scissors"
+                    break;
+                case "Paper":
+                    return "you win! scissors beats paper"
+                    break;
+                default:
+                    return "tie, you both chose scissors "
+            }
+            break;
+        default: 
+            return "Player did not make a selection"
+    }
+}
+
+   
+
+  const playerSelection = "ROcK";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
