5c4c8b65e90ea5fc0ca8924563d78957b4ebca0e
Max Lopez <familiacachorin@gmail.com>
2023-06-08 13:05:03 -0700

    create getComputerChoice function

---
+++ game.js
@@ -0,0 +1,13 @@
+
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        default:
+            return "Scissors";
+    }
+}
