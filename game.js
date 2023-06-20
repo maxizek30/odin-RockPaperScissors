d835e56aea4b9cf6554a54a0864c23f3ba880a6a
Max Lopez <familiacachorin@gmail.com>
2023-06-19 18:03:27 -0700

    finish the function of the rps game

--- game.js
+++ game.js
@@ -1,3 +1,19 @@
+//add event listeners to buttons
+let btns = document.querySelectorAll('button');
+for (i of btns) {
+    i.addEventListener('click', game);
+  }
+//keep count of stuff
+let playerCount = 0;
+let compCount = 0;
+
+
+
+
+
+
+
+
 
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
@@ -19,30 +35,36 @@ function playRound(playerSelection, computerSelection) {
                     return "tie you both chose rock"
                     break;
                 case "Paper":
+                    compCount++;
                     return "You lose! paper beats rock"
                     break;
                 default:
+                    playerCount++;
                     return "You win! rock beats scissors"
             }
             break;
         case "paper":
             switch (computerSelection) {
                 case "Rock":
+                    playerCount++;
                     return "You win! paper beats rock"
                     break;
                 case "Paper":
                     return "tie you both chose paper"
                     break;
                 default:
+                    compCount++;
                     return "You lose, Scissors beats paper"
             }
             break;
         case "scissors":
             switch (computerSelection) {
                 case "Rock":
+                    compCount++;
                     return "You lose, rock beats scissors"
                     break;
                 case "Paper":
+                    playerCount++;
                     return "you win! scissors beats paper"
                     break;
                 default:
@@ -53,12 +75,30 @@ function playRound(playerSelection, computerSelection) {
             return "Player did not make a selection"
     }
 }
-function game() {
-    console.log("ok we will play this game 5 times");
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What's your choice (Rock, paper or scissors)?");
-        console.log(playRound(playerSelection, getComputerChoice()));
+function game(e) {
+    //get playerSelection from click
+    const playerSelection = e.target.getAttribute('id');
+
+    //play a round 
+    let str = playRound(playerSelection, getComputerChoice());
+    const result = document.querySelector('.Result');
+    result.innerHTML = str;
+
+    //print scores
+    const scores = document.querySelector('.Scores');
+    scores.innerHTML = "You: " + playerCount + " Computer: " + compCount;
+
+    //if someone won!
+    if (compCount == 5) {
+        const winner = document.querySelector(".Winner");
+        winner.innerHTML = "Take the L, the computer is the winner and will take your job";
     }
-    console.log("we are done here");
-}
-game();
+    if (playerCount == 5) {
+        const winner = document.querySelector(".Winner");
+        winner.innerHTML = "Big W, the computer has nothing on you"
+    }
+};
+
+
+
+
